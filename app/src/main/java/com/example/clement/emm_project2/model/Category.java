3fafe548d94bf71bf9c16ac6d2a1d36c03882da5
hugohil <hilairehugo@gmail.com>
2015-08-10 16:05:54 +0200

    Create category and sub-category model

--- app/src/main/java/com/example/clement/emm_project2/model/Category.java
+++ app/src/main/java/com/example/clement/emm_project2/model/Category.java
@@ -1,8 +1,90 @@
 package com.example.clement.emm_project2.model;
 
+import android.media.Image;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Clement on 10/08/15.
  */
 public class Category {
+    @JsonProperty("_id")
+    private String id;
 
+    @JsonProperty("tid")
+    private int tid;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("image_url")
+    private String imageURL;
+
+    @JsonProperty("active")
+    private boolean active;
+
+    @JsonProperty("subcategories")
+    private List<SubCategory> subCategories;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public List<SubCategory> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(List<SubCategory> subCategories) {
+        this.subCategories = subCategories;
+    }
 }
