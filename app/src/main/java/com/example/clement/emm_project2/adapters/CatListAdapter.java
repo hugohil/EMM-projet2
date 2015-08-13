960d4b499651afd1a9523c645aff283f390d612b
hugohil <hilairehugo@gmail.com>
2015-08-13 13:14:01 +0200

    use ViewHolder pattern

--- app/src/main/java/com/example/clement/emm_project2/adapters/CatListAdapter.java
+++ app/src/main/java/com/example/clement/emm_project2/adapters/CatListAdapter.java
@@ -17,8 +17,9 @@ import java.util.ArrayList;
  * Created by perso on 11/08/15.
  */
 public class CatListAdapter extends ArrayAdapter<Category> {
-
-    private final static String TAG = CatListAdapter.class.getSimpleName();
+    private static class ViewHolder{
+        TextView title;
+    }
 
     public CatListAdapter(Context context, ArrayList<Category> catList) {
         super(context, 0, catList);
@@ -26,12 +27,17 @@ public class CatListAdapter extends ArrayAdapter<Category> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder viewHolder;
         if(convertView == null){
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.category_row, parent, false);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.cat_row_title);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        TextView title = (TextView) convertView.findViewById(R.id.cat_row_title);
-        title.setText(getItem(position).getTitle());
+        viewHolder.title.setText(getItem(position).getTitle());
 
         return convertView;
     }
