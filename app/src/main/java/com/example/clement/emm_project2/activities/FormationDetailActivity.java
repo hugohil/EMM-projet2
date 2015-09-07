26377774b844d823f831cd7be60603f5f576000d
hugohil <hilairehugo@gmail.com>
2015-09-07 14:55:48 +0200

    add drawer to FormationSummary and FormationDetail

--- app/src/main/java/com/example/clement/emm_project2/activities/FormationDetailActivity.java
+++ app/src/main/java/com/example/clement/emm_project2/activities/FormationDetailActivity.java
@@ -14,9 +14,14 @@ import android.widget.ExpandableListView;
 import com.example.clement.emm_project2.R;
 import com.example.clement.emm_project2.adapters.ItemListAdapter;
 import com.example.clement.emm_project2.app.App;
+import com.example.clement.emm_project2.app.drawer.DrawerActivity;
+import com.example.clement.emm_project2.app.drawer.DrawerItem;
+import com.example.clement.emm_project2.app.drawer.DrawerSection;
+import com.example.clement.emm_project2.app.drawer.DrawerSectionItem;
 import com.example.clement.emm_project2.app.server.ResponseHandler;
 import com.example.clement.emm_project2.app.server.ServerHandler;
 import com.example.clement.emm_project2.data.DataAccess;
+import com.example.clement.emm_project2.model.Category;
 import com.example.clement.emm_project2.model.Formation;
 import com.example.clement.emm_project2.model.Item;
 import com.example.clement.emm_project2.util.JsonUtil;
@@ -26,10 +31,11 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
-public class FormationDetailActivity extends AppCompatActivity {
+public class FormationDetailActivity extends DrawerActivity {
 
     private final static String TAG = FormationDetailActivity.class.getSimpleName();
     public ItemListAdapter adapter;
@@ -38,20 +44,47 @@ public class FormationDetailActivity extends AppCompatActivity {
     HashMap<Item, List<Item>> videos = new HashMap<Item, List<Item>>();
     private Context context = this;
     private SharedPrefUtil sharedPref = new SharedPrefUtil();
+    private Formation formation = new Formation();
+    private List<Category> categories = new ArrayList<Category>();
+    private DataAccess dataAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formation_detail);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
             final String eanCode= extras.getString("ean");
             DataAccess da = new DataAccess(App.getAppContext());
             List<Formation> formations = da.findDataWhere(Formation.class, "ean", eanCode);
-            Formation formation = formations.get(0);
+            formation = formations.get(0);
             displayFormation(formation);
         }
+
+        ArrayList<DrawerItem> menuItems = new ArrayList<DrawerItem>();
+        List<Category> dbCategories;
+        dataAccess = new DataAccess(this);
+        dbCategories = dataAccess.getAllDatas(Category.class);
+        Collections.sort(dbCategories, new Comparator<Category>() {
+            public int compare(Category c1, Category c2) {
+                String t1 = c1.getTitle().toUpperCase();
+                String t2 = c2.getTitle().toUpperCase();
+                return t1.compareTo(t2);
+            }
+        });
+        if(SharedPrefUtil.areFavoriteFormations()) {
+            menuItems.add(DrawerSection.create(300, "Navigation", "ic_action_label", this));
+            menuItems.add(DrawerSectionItem.create(301, "Favoris", true));
+            menuItems.add(DrawerSectionItem.create(302, "Parcours", true));
+        }
+        menuItems.add(DrawerSection.create(200, "Catégories", "ic_action_bookmark", FormationDetailActivity.this));
+        for(Category category : dbCategories) {
+            menuItems.add(DrawerSectionItem.create(dbCategories.indexOf(category), category.getTitle(), true));
+        }
+        menuItems.add(DrawerSection.create(100, "Configuration", "ic_action_settings", FormationDetailActivity.this));
+        menuItems.add(DrawerSectionItem.create(101, "Preferences", true));
+        setDrawerContent(menuItems);
+        categories.addAll(dbCategories);
     }
 
     public void displayFormation(final Formation formation) {
@@ -65,12 +98,12 @@ public class FormationDetailActivity extends AppCompatActivity {
             public boolean onChildClick(ExpandableListView parent, View v,
                                         int groupPosition, int childPosition, long id) {
                 Item item = videos.get(chapters.get(groupPosition)).get(childPosition);
-                Log.d(TAG, "item.mongoID: "+item.getMongoID());
+                Log.d(TAG, "item.mongoID: " + item.getMongoID());
                 String ids = formation.getEan() + "," + item.getMongoID(); // My eyes ... they hurt :'( !
                 sharedPref.addStartedVideo(ids);
-                Log.d(TAG, "FIELD VIDEO =>"+item.getFieldVideo().get(0).get("filepath"));
+                Log.d(TAG, "FIELD VIDEO =>" + item.getFieldVideo().get(0).get("filepath"));
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.parse((String)item.getFieldVideo().get(0).get("filepath")), "video/*");
+                intent.setDataAndType(Uri.parse((String) item.getFieldVideo().get(0).get("filepath")), "video/*");
                 context.startActivity(intent);
 
                 return false;
@@ -115,5 +148,39 @@ public class FormationDetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void resetTitle(){
+        setTitle(formation.getTitle());
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_formation_detail;
+    }
+
+    @Override
+    protected void onNavItemSelected(int id) {
+        if(id < 100 ){
+            // Click on categories
+            Category cat = categories.get(id);
+
+            Intent i = new Intent(this, SubCatActivity.class);
+            i.putExtra("desc", cat.getDescription());
+            i.putExtra("title", cat.getTitle());
+            i.putExtra("catId", cat.getMongoID());
+
+            startActivity(i);
+        }
+        if(id > 300){
+            if(id > 301){
+                Intent i = new Intent(this, StartedVideosActivity.class);
+                startActivity(i);
+            } else {
+                Intent i = new Intent(this, FavoriteActivity.class);
+                startActivity(i);
+            }
+        }
     }
 }
